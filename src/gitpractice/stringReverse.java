e02cd80baa1c0e833c2d2a878620f4ed55a11cd7
sonalisubhashmore <82300621+sonalisubhashmore@users.noreply.github.com>
2021-06-04 17:24:32 +0530

    Update stringReverse.java

--- src/gitpractice/stringReverse.java
+++ src/gitpractice/stringReverse.java
@@ -6,7 +6,7 @@ public class stringReverse
 	public static void main(String[] args) 
 	{
 	
-		String s1="sonali";
+		String s1="sonalisubhashmore";
 		String rev="";
 		
 		for(int i=s1.length()-1; i>=0; i--)
