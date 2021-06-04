6f24c9f743a12370cf765cb300a66e9d67c96a0f
admin <admin@DESKTOP-5FDGF9B>
2021-06-04 17:13:25 +0530

    First commite

---
+++ src/gitpractice/stringReverse.java
@@ -0,0 +1,19 @@
+package gitpractice;
+
+public class stringReverse 
+{
+
+	public static void main(String[] args) 
+	{
+	
+		String s1="sonali";
+		String rev="";
+		
+		for(int i=s1.length()-1; i>=0; i--)
+		{
+			rev=rev+s1.charAt(i);
+		}
+		System.out.println(rev);
+		
+	}
+}
